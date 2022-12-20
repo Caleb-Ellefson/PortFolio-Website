e8f3ae14665fbd4af742ab441e8322b54024b605
caleb_ellefson <calebroger@gmail.com>
2022-12-20 15:08:46 -0600

    Icons

--- my-app/src/Components/Nav.js
+++ my-app/src/Components/Nav.js
@@ -1,21 +1,29 @@
 
 import React from "react";
 
+import { Container } from 'react-bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithub, faLinkedin, } from '@fortawesome/free-brands-svg-icons'
+
 export default function Nav () {
     return ( 
+        <div>
+            <Container>
+                <div>
+                    <a href="https://github.com/Caleb-Ellefson">
+                        <FontAwesomeIcon icon={faGithub} />
+                    </a>
+                    <a href="https://www.linkedin.com/in/caleb-ellefson/">
+                        <FontAwesomeIcon icon={faLinkedin} />
+                    </a>
+                    <a href="https://github.com/Caleb-Ellefson">
+                        <FontAwesomeIcon icon={faGithub } />
+                    </a>
+                </div>
+            </Container>
+        </div>
 
-        <nav id='centered'className="nav">
-            <div>
-                <a className="nav-logo"> Ellefson.Dev </a>
-            </div>
-
-            <ul className="nav-child">
-                <li><a className="nav-link-home" href="#tiles"></a> // home  </li>
-                <li><a className="nav-link-experience" href="#tiles"></a> // work </li>
-                <li><a className="nav-link-about" href="#tiles"></a> // about </li>
-                <li><a className="nav-link-contact" href="#tiles"></a> // contact </li>
-            </ul>
-        </nav>
 
     )
 }
